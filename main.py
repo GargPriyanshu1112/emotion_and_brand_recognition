bc773bf46721a3a2aa94491420c4db62e890b125
Priyanshu Garg <92866633+GargPriyanshu1112@users.noreply.github.com>
2024-11-13 23:30:27 +0530

    show class names instead of labels.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import time
-import random
+import json
 import streamlit as st
 from transformers import pipeline
 
+def get_corresponding_cls(cls_to_lbl_map, pred_lbl):
+    lbl_to_cls_map = {lbl: cls for cls, lbl in cls_to_lbl_map.items()}
+    return lbl_to_cls_map[pred_lbl]
 
 # Streamlit app
-def main():
+def main(mappings):
     # Page title
     st.title("Emotion Analyzer")
     # User input
@@ -13,29 +15,43 @@ def main():
 
     if st.button("Submit"):
         if input_text:
-            with st.spinner("Loading models and processing..."):
+            with st.spinner(
+                "Loading models... This will take a moment, and only happens once."
+            ):
                 emotion_recog_model = pipeline(
                     "text-classification", model="PriyHF/emotion_recog"
                 )
-                brand_product_category_recog_model = pipeline(
+                brand_product_recog_model = pipeline(
                     "text-classification", model="PriyHF/brand_product_recog"
                 )
 
-                emotion_pred = emotion_recog_model(input_text)
-                st.subheader("Predicted Emotion: ")
-                st.write(emotion_pred)
+                emotion_pred_dict = emotion_recog_model(input_text)[0]
+                emotion_pred_lbl = emotion_pred_dict["label"]
+                emotion_pred_lbl = int(emotion_pred_lbl.split("_")[-1])
+                emotion_pred_cls = get_corresponding_cls(
+                    mappings["emotions"], emotion_pred_lbl
+                )
 
-                if emotion_pred:
-                    brand_product_category_pred = brand_product_category_recog_model(
-                        input_text
+                st.subheader("Predicted Emotion: ")
+                st.write(emotion_pred_cls)
+
+                if emotion_pred_cls in ["Positive emotion", "Negative emotion"]:
+                    brand_prod_pred_dict = brand_product_recog_model(input_text)[0]
+                    brand_prod_pred_lbl = brand_prod_pred_dict["label"]
+                    brand_prod_pred_lbl = int(brand_prod_pred_lbl.split("_")[-1])
+                    brand_product_pred_cls = get_corresponding_cls(
+                        mappings["categories"], brand_prod_pred_lbl
                     )
                     st.subheader("Category: ")
-                    st.write(brand_product_category_pred)
-                else:
-                    st.warning("Input classified as Type B")
+                    st.write(brand_product_pred_cls)
         else:
             st.error("Please enter some text!")
 
 
 if __name__ == "__main__":
-    main()
+    # Get mappings
+    with open("mapping.json", "r") as file:
+        mappings = json.load(file)
+    file.close()
+    # Initiate
+    main(mappings)
